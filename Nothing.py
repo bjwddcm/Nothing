b46dcafaf474d4d2d543aea9a0f69c9782b2d3ad
zyh <253133052@qq.com>
2022-12-29 18:07:45 +0800

    2022年12月29日18:07:35

--- Nothing.py
+++ Nothing.py
@@ -1,17 +1,5 @@
-# 创建字典方法
-a = {"name": 12}
-b = [['name', 12]]
+from typing import *
+a = {1, 23}
 
-c = list(range(1, 10))
-d = dict.fromkeys(c, 12)
-# print(d)
-
-keys = [1, 2, 3]
-values = [1, 2, 3]
-e = dict(zip(keys, values))
-print(e)
-# **************************************
-
-print(e.keys())
-print(e.values())
-print(e.items())
+print(a, type(a))
+print(isinstance(a, Iterable))
