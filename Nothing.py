128a6e113c97d5305065a332d6fa93dd6f0a1d7b
zyh <253133052@qq.com>
2022-12-29 16:05:51 +0800

    2022年12月29日16:05:47

---
+++ Nothing.py
@@ -0,0 +1,13 @@
+# 创建字典方法
+a = {"name": 12}
+b = [['name', 12]]
+
+c = list(range(1, 10))
+d = dict.fromkeys(c, 12)
+# print(d)
+
+keys = [1, 2, 3]
+values = [1, 2, 3]
+e = dict(zip(keys, values))
+print(e)
+# **************************************
