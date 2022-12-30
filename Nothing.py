f707b0f7220ceed8dd38e82c1c8ba90cffcd2d0b
zyh <253133052@qq.com>
2022-12-30 19:56:11 +0800

    q
    q

--- Nothing.py
+++ Nothing.py
@@ -1,9 +1,9 @@
 import copy
 
-a = (1,2,3,4)
+a = (1, 2, 3, 4)
 b = copy.copy(a)
 c = tuple(a)
 
-print(id(a)==id(b))
-print(id(a)==id(c))
+print(id(a) == id(b))
+print(id(a) == id(c))
 print(type(a))
